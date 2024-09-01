ac96c12c5d48a80a05bdbab7d0f26ee77ace72f0
kwon <rnjsdndud00@naver.com>
2024-09-01 23:54:53 +0900

    spring security

--- src/main/java/com/date/coffee/controller/MemberController.java
+++ src/main/java/com/date/coffee/controller/MemberController.java
@@ -4,17 +4,11 @@ import com.date.coffee.domain.Member;
 import com.date.coffee.dto.MemberDto;
 import com.date.coffee.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Collection;
-import java.util.Iterator;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,20 +20,6 @@ public class MemberController {
     public String signUpForm(Model model, @RequestParam(value = "error", required = false)String error,
                              @RequestParam(value = "exception", required = false)String exception) {
         model.addAttribute("memberDto", new MemberDto());
-
-        model.addAttribute("error", error);
-        model.addAttribute("exception", exception);
-
-        String id = SecurityContextHolder.getContext().getAuthentication().getName();
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
-        GrantedAuthority auth = iter.next();
-        String role = auth.getAuthority();
-
-        model.addAttribute("id", id);
-        model.addAttribute("role", role);
 
         return "member/memberSignUp";
     }
